80de073dded5e19ba6d4b261ceba218e3a4c88bc
Yukinari Toyota <xxseyxx@gmail.com>
2013-07-29 09:55:34 +0900

    Add comments.

--- init.rb
+++ init.rb
@@ -1,10 +1,9 @@
 Redmine::Plugin.register :redmine_macros_textile do
-  name 'Redmine Macros Textile plugin'
+  name 'Redmine Textile macro plugin'
   author 'Yukinari Toyota'
   description 'This plugin adds textile formatting macro.'
   version '0.0.1'
-  url 'http://github.com/t-yuki/redmine_macros_textile'
-  author_url 'http://github.com/t-yuki'
+  url 'https://github.com/t-yuki/redmine_macros_textile'
 end
 
 require 'redmine/wiki_formatting/textile/formatter'
