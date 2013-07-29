3f64a049788e8999ded4b76ae909f0e4b7822124
Yukinari Toyota <xxseyxx@gmail.com>
2013-07-29 09:17:49 +0900

    initial import

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_macros_textile do
+  name 'Redmine Macros Textile plugin'
+  author 'Yukinari Toyota'
+  description 'This plugin adds textile formatting macro.'
+  version '0.0.1'
+  url 'http://github.com/t-yuki/redmine_macros_textile'
+  author_url 'http://github.com/t-yuki'
+end
